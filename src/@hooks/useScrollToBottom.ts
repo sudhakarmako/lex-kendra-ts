0e6cc8d3b35d35a2ecfa2ec97bd58517019cc169
sudhakarmako <sudhakar@makoitlab.com>
2023-02-02 18:12:31 +0530

    handling animations

--- src/@hooks/useScrollToBottom.ts
+++ src/@hooks/useScrollToBottom.ts
@@ -3,8 +3,11 @@ import { useRef } from "react";
 export const useScrollToBottom = () => {
   const element = useRef<any>(null);
 
-  const scrollToBottom = () =>
-    (element.current.scrollTop = element?.current?.scrollHeight);
+  const scrollToBottom = () => {
+    setTimeout(() => {
+      element.current.scrollTop = element?.current?.scrollHeight;
+    }, 100);
+  };
 
   return {
     element,
